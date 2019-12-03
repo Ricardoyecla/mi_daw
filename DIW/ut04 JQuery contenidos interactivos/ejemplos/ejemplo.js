53fab4a6aa4fdcf38f18579cf509595672fef48b
ricardoYecla <ricardoyecla@gmail.com>
2019-12-03 18:52:28 +0100

    jquery

---
+++ DIW/ut04 JQuery contenidos interactivos/ejemplos/ejemplo.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function(){
+    //Añadir clases y eleminar clases
+    $("h1").addClass("bg-primary");
+    $("p:first").removeClass("display-1").addClass("bg-info");
+    $("p:last").removeClass("display-1").addClass("bg-warning");
+    
+    //Añadir elemento en linea al principio y al final de un contenedor
+    
+    $(".container").append("<p>Hola soy nuevo</p>")
+    var contendor =$(".container");
+    
+    contendor.prepend("<p>Un pe al principio</p>");
+    
+    
+    //cambiar atributos css
+    
+    $(".colorea").css("color","red");
+   $(".colorea").css("color","red").css("text-align","center"); $(".colorea").css({backgroundColor:"yellow",fontSize:"3em"});
+    
+    //eleminar un elemento.
+    
+    $(".colorea p:last").remove();
+    
+    //Ocultar un elemento.
+    $(".colorea p:first").hide();
+    
+    //Trabajar con los atributos
+    
+    $("img").attr("src","https://cloud10.todocoleccion.online/barbie-ken-accesorios/tc/2016/09/04/13/59689751.webp").attr("width","50");
+    // Gestión de eventos
+    
+    var parrafo=$("#parrafo");
+    
+    $("#au").click(function(){
+        parrafo.addClass("display-4");    
+    })
+    $("#di").click(function(){
+        parrafo.removeClass("display-4");    
+    })
+    $("#in").click(function(){
+        parrafo.toggleClass("display-4");    
+    })
+    
+    var inText=$("input");
+    $("input").keyup(function(){
+        $("#contenido").html(inText.val());
+    })
+    
+    
+})
