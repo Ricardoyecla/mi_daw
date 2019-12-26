dcb19e08e06459ee08099346eceb75bbe4bb3b86
RicardoYecla <ricardoyecla@gmail.com>
2019-12-27 00:44:10 +0100

    Todos los ejemplos JQuery hasta animaciones

--- DIW/ut04 JQuery contenidos interactivos/ejemplos/ejemplo.js
+++ DIW/ut04 JQuery contenidos interactivos/ejemplos/ejemplo.js
@@ -1,75 +1,78 @@
 
 $(document).ready(function(){
     //Añadir clases y eleminar clases
-    $("h1").addClass("bg-primary");
+    $("h1").addClass("bg-warning");
     $("p:first").removeClass("display-1").addClass("bg-info");
     $("p:last").removeClass("display-1").addClass("bg-warning");
     
     //Añadir elemento en linea al principio y al final de un contenedor
     
-    $(".container").append("<p>Hola soy nuevo</p>")
-    var contendor =$(".container");
+    $(".lista").append("<li>Hola soy nuevo</li>")
+    var contendor =$(".lista");
     
-    contendor.prepend("<p>Un pe al principio</p>");
+    contendor.prepend("<li>Un pe al principio</li>");
     
     
     //cambiar atributos css
     
-    $(".colorea").css("color","red");
-   $(".colorea").css("color","red").css("text-align","center"); $(".colorea").css({backgroundColor:"yellow",fontSize:"3em"});
+    $("#colorea1").css("color","red");
+    $("#colorea2").css("color","red").css("text-align","center"); $("#colorea3").css({backgroundColor:"yellow",fontSize:"2em"});
     
     //eleminar un elemento.
     
-    $(".colorea p:last").remove();
+    $(".desaparece li:last").remove();
     
     //Ocultar un elemento.
-    $(".colorea p:first").hide();
+    $(".desaparece li:first").hide();
     
     //Trabajar con los atributos
     
     $("img").attr("src","https://cloud10.todocoleccion.online/barbie-ken-accesorios/tc/2016/09/04/13/59689751.webp").attr("width","50");
+    
+    
     // Gestión de eventos
     
     // Ampliamos el tamaño del contenido del div
     var parrafo=$("#parrafo");
     
-    $("#au").click(function(){
-        parrafo.addClass("display-4");    
+    $("#gordo").click(function(){
+        parrafo.addClass("display-4");  
     })
-    $("#di").click(function(){
+    $("#fino").click(function(){
         parrafo.removeClass("display-4");    
     })
-    $("#in").click(function(){
+    $("#cambia").click(function(){
         parrafo.toggleClass("display-4");    
     })
     
     //Añadimos lo que se teclea en el input al h1
     var inText=$("input");
     $("input").keyup(function(){
-        $("#contenido").html(inText.val());
+        $("#parrafo").html(inText.val());
     })
     
     // Añadimos lo que se teclea a una lista
     $("#btA").click(function(e){
         e.preventDefault();
-        $("#lista").append('<li>'+$("#nombre").val() +'</li>')
+        $(".lista").append("<li>"+$("input").val() +"</li>")
     })
     
-    // Ocultamos o mostramos un div
-    var parrafo1=$("#parrafo1");
+    // Ocultamos o mostramos un parrafo 
+    var parrafo=$("#parrafo");
     
     $("#au1").click(function(){
-        $("#parrafo1").hide(1000)   
+        $("#parrafo").hide(1000)   
     });
     $("#di1").click(function(){
-        parrafo1.show("slow");  
+        parrafo.show("slow");  
     });
     $("#in1").click(function(){
-        parrafo1.toggle(2000);    
+        parrafo.toggle(2000);    
     });
     
     //Hacemos aparecer y desaparecer los div con animación
-    $("#bAnimar").click(function(){
+    $("#An1").click(function(){
+      
         $(".mis_cuadrados div:first").fadeOut(2000);
         $(".mis_cuadrados div:nth-child(2)").fadeOut(2500);
         $(".mis_cuadrados div:last").fadeOut(3000);
@@ -81,7 +84,7 @@ $(document).ready(function(){
     });
     
     // Animamos un botón para que desplace
-    $("#bMove").click(function(){
+    $("#An2").click(function(){
         $(".cuadrado2").animate({left:"200px",opacity: '0.5',
     height: '150px',
     width: '150px',
